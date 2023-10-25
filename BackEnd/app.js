5b2a483520b622ac4835baa6ec52b913684cd431
Belin27000 <y.lecerf@protonmail.com>
2023-10-25 17:50:58 +0200

    CONFIG: mongoDB configuration

--- BackEnd/app.js
+++ BackEnd/app.js
@@ -1,9 +1,32 @@
 const express = require('express');
+require('dotenv').config()
 
+const mongoose = require('mongoose')
 const app = express();
+mongoose.connect('mongodb+srv://belin:<process.env.SECRET_KEY>@cluster0.xsfqrpi.mongodb.net/?retryWrites=true&w=majority',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
 
+app.use((req, res, next) => {
+    console.log("requete reçu");
+    next()
+})
+
+app.use((req, res, next) => {
+    res.status(201);
+    next()
+})
+
+app.use((req, res, next) => {
+    res.json({ message: 'votre requete a bien ete reçu' });
+    next()
+})
 app.use((req, res) => {
-    res.json({ message: 'votrre requete a bien ete reçu' })
+    console.log('Reponse envoye avec success');
 })
 
 module.exports = app;
